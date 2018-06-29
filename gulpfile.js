72ae7bb2d64cee890646634c251b651a09730a93
italo fontes <italo5123@hotmail.com>
2018-06-29 18:20:16 -0300

    gulp fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
-var gulpif = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var concat = require('gulp-concat');
 var browserSync = require('browser-sync').create();
 
 
 var stylesFolder = './src/sass/**/*.scss';
+var scriptsFolder = './src/js/**/*.js';
 var pagesFolder = './src/*.html';
 
 gulp.task('sass', function(){
@@ -14,15 +16,31 @@ gulp.task('sass', function(){
     .pipe(sass({
         outputStyle:'compressed'
     }).on('error', sass.logError))
+    .pipe(cssnano())
     .pipe(gulp.dest('./docs/css'))
     .pipe(browserSync.stream());
 });
 
-gulp.task('useref', function(){
+
+gulp.task('html', function(){
     return gulp.src(pagesFolder)
-    .pipe(useref())
-    .pipe(gulpif('*.js', uglify()))
-    .pipe(gulp.dest('./docs'));
+    .pipe(gulp.dest('./docs'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('buildjs', function(){
+    return gulp.src('./src/js/*.js')
+    .pipe(concat('build.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./docs/js'))
+    .pipe(browserSync.stream())
+});
+
+gulp.task('images', function(){
+    gulp.src('src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('docs/img'))
+        .pipe(browserSync.stream())
 });
 
 gulp.task('server', function(){
@@ -32,5 +50,6 @@ gulp.task('server', function(){
         }
     });
     gulp.watch(stylesFolder,['sass']);
-    gulp.watch('./src/*.html' );
+    gulp.watch(pagesFolder,['html'] );
+    gulp.watch(scriptsFolder, ['buildjs']);
 });
