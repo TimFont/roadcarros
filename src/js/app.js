9afca550a653aa58611ad6e1036543fb6e6ff991
italo fontes <italo5123@hotmail.com>
2018-07-24 21:53:45 -0300

    component slider added

--- src/js/app.js
+++ src/js/app.js
@@ -6,27 +6,27 @@ const Slider = require('./components/Slider');
 
 
 window.onload = () => {
-     //roda slide
-    if(screen.width > 500){
+    //roda slide
+    if (screen.width > 500) {
 
         const slidesContent = [
-            {text: 'test1, best car ever', img:'./img/slide1.jpg'},
-            {text: 'test2, more velocity', img:'./img/slide2.jpg'},
-            {text: 'test2, more velocity', img:'./img/slide2.jpg'}
+            { text: 'test1, best car ever', img: './img/slide1.jpg' },
+            { text: 'test2, more velocity', img: './img/slide2.jpg' },
+            { text: 'test2, more velocity', img: './img/slide3.jpg' }
         ];
         const mainSlider = new Slider(elements.mainHeader, slidesContent);
-            mainSlider.render();
-            mainSlider.start();
-
-   
-
-        /*setTimeout(()=>{
-            //mainSlider.setSlidePos(1, -100, -1);
-            mainSlider.slideLoop();
-           // mainSlider.setSlidePos(1, -100, -1, slides);
-        }, 4000);*/
+        mainSlider.render();
+        mainSlider.start(5000);
 
 
+
+
+        window.onscroll = function () {
+            const navigationClasses = elements.navBar.classList;
+            pageYOffset > 200 ?
+                navigationClasses.add('main-nav--scrolled') :
+                navigationClasses.remove('main-nav--scrolled');
+        };
 
 
     }
@@ -35,10 +35,8 @@ window.onload = () => {
 
     //não roda
     else if (screen.width < 500) {
-        for(let i = 1; i <= 3; i++){
-            elements.slider.innerHTML +=  `<div class="home-slider__slide home-slider__slide--1">a</div>`;
-        }
-   
+        elements.mainHeader.style.backgroundImage = 'url(./img/slide1.jpg)';
+
     }
 
 
@@ -46,96 +44,3 @@ window.onload = () => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*window.onscroll = function(){
-    const navigationClasses = elements.navBar.classList;
-    pageYOffset > 200 ?
-        navigationClasses.add('main-nav--scrolled') :
-        navigationClasses.remove('main-nav--scrolled');
-};
-
-
-
-
-window.onload = function(){
-    slider.startSlider();
-}
-*/
-/*========SLDER======*/
-
-/*
-const slider = {
-    element: elements.slider,
-    sliders: elements.slider.querySelectorAll('.home-slider__slide'),
-    slideIndex: 1,
-    slideTimer: 7000,
-
-    startSlider(){
-        setInterval(() =>{
-            this.slideLoop();
-        }, slider.slideTimer);
-    },
-
-    setSlidePos (index, position, z){
-        this.sliders[index -1].style.right = `${position}%`;
-        this.sliders[index -1].style.zIndex = `${z}`;
-    },
-
-    slideLoop (){
-        const slidersLength = this.sliders.length;
-        let nextIndex =  this.slideIndex === slidersLength ?
-            1 : this.slideIndex + 1;
-
-        let nextItem = this.sliders[nextIndex - 1];
-        nextItem.style.opacity = '0';
-
-        setTimeout(() => {
-            for(let count = 1; count <= slidersLength; count++){
-
-                if(count === this.slideIndex){
-                    this.setSlidePos(count, 0, -2);
-
-                }
-                else if (count === nextIndex) {
-                    this.setSlidePos(count, -100, -1);
-                }
-                else {
-                    this.setSlidePos(count, 100, -3);
-                }
-            }
-            this.slideIndex = nextIndex;
-
-        }, 500);
-
-        setTimeout(() => {
-            nextItem.style.opacity = '1';
-        }, 1000);
-
-    }
-};
-*/
-
-/*if(screen.width < 700) {
-    elements.slider.innerHTML='<div class="home-slider__slide home-slider__slide--1">a</div>';   
-}else {
-    console.log(screen.width);
-    for (let index = 1; index <= 3; index++){
-        elements.slider.innerHTML += `<div class="home-slider__slide home-slider__slide--${index}">a</div>`;
-    }
-}*/
--- src/js/components/Slider.js
+++ src/js/components/Slider.js
@@ -2,8 +2,16 @@ class Slider {
     constructor(parent, content){
         this.parent = parent;
         this.content = content;
+        this.state = 0;
+        this.slides = [];
     }
 
+    start(interval){
+        setInterval(()=> {
+            this.nextState();
+        }, interval);
+
+    }
 
 
     render(){
@@ -22,14 +30,57 @@ class Slider {
             slider.innerHTML += `
                 <div class = "slider__slide slider__slide--${position}">
                     <div class = "slider__image" style = "background-image: url(${element.img})"></div>
-                    <h1 class = "slider__text>${element.text}</h1>
+                    <h1 class = "slider__text">${element.text}</h1>
                 </div> 
             `;
+
         }
-        console.log(this.parent);
+
         this.parent.insertBefore(slider, this.parent.firstChild);
+        this.slides.push(...this.parent.querySelectorAll('.slider__slide'));
         
 
+    }
+
+    nextState (){
+        const slidesLength = this.content.length;
+        let nextIndex = (this.state === (slidesLength - 1)) ? 0 : this.state + 1,
+            beyondIndex = nextIndex === (slidesLength - 1) ? 0 : nextIndex + 1,
+            beyondItem = this.slides[beyondIndex];
+            beyondItem.style.opacity = '0';
+
+        setTimeout(() => {
+            for(let count = 0; count < 3; count++){
+                let currentItem = this.slides[count];
+                console.log(currentItem);
+
+                if(count === this.state){
+                    Slider.transition(currentItem, 'beyond', 'primary');
+        
+                }
+                else if (count === nextIndex) {
+                    Slider.transition(currentItem, 'primary', 'next');
+                }
+                else {
+                    Slider.transition(currentItem, 'next', 'beyond');
+
+                }
+            }
+
+        this.state = nextIndex;
+        
+        }, 500);
+        
+        setTimeout(() => {
+            beyondItem.style.opacity = '1';
+        }, 1000);
+
+        
+    }
+
+    static transition(item, add, remove) {
+        item.classList.add(`slider__slide--${add}`);
+        item.classList.remove(`slider__slide--${remove}`);
     }
 }
 
